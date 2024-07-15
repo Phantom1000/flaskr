43913650b6a3f12683c57083b23c51d782b9ec0a
Phantom <nikita.dankin10@gmail.com>
2024-07-16 00:42:39 +0300

    add templates

---
+++ flaskr/auth.py
@@ -0,0 +1,97 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        query = 'SELECT * FROM user WHERE id = ?'
+        g.user = get_db().execute(query, (user_id,)).fetchone()
+
+
+@bp.route("/")
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Введите имя пользователя'
+        elif not password:
+            error = 'Введите пароль'
+
+        if error is None:
+            try:
+                stmt = "INSERT INTO user (username, password) VALUES (?, ?)"
+                db.execute(stmt, (username, generate_password_hash(password)))
+                db.commit()
+            except db.IntegrityError:
+                error = f"Пользователь {username} уже существует"
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        query = 'SELECT * FROM user WHERE username = ?'
+        user = db.execute(query, (username,)).fetchone()
+
+        if user is None:
+            error = 'Проверьте имя пользователя'
+        elif not check_password_hash(user['password'], password):
+            error = 'Проверьте пароль'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('auth.index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.index'))
